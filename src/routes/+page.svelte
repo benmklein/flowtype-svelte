d0cb1132bf2e3260c6c2bccdedd1ce51b12e7fa3
Ben Klein <benmklein@gmail.com>
2024-07-12 20:25:09 -0700

    path adjustment

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts" type='module'>
 import * as svelte from 'svelte'
 import * as Ace from "ace-builds"
-Ace.config.set('basePath', "../../node_modules/ace-builds/src-min-noconflict/")
+Ace.config.set('basePath', "/node_modules/ace-builds/src-min-noconflict/")
 let editor : Ace.Ace.Editor;
 let goaleditor : Ace.Ace.Editor;
 let score = 0;
